abf191603dec4ed526095e330c9b8a5a2ffcae68
Luis Fernando <luiisfdrlv@gmail.com>
2025-11-05 21:55:41 +0100

    Tutoría 30/10/2025

--- Clase30102025/Ejercicio4.py
+++ Clase30102025/Ejercicio4.py
@@ -1,19 +1,28 @@
 import os
 
 '''
-1. Comprobar que existe el archivo
-2. Comprobar que sea un archivo
-3. Comprobar que exista la nueva ruta
-4. Comprobar que la nueva ruta es un directorio
-5. Comprobar que path y new_path no son iguales
-6. Mover el archivo a new_path
+1. Comprobar que sea un archivo
+2. Comprobar que la nueva ruta es un directorio
+3. new_path = directorio/nombre_fichero
+4 Comprobar que path y new_path no son iguales
+5. Mover el archivo a new_path
 '''
 
 
 def move_file(path, new_path):
+    path_without_filename = new_path
+    new_filename = ""
     if not os.path.isfile(path):
+        raise FileNotFoundError("No existe el archivo")
+    if not os.path.isdir(new_path):
+        path_without_file = new_path.rsplit("/",maxsplit=1)[0]
+        new_filename = new_path.rsplit("/",maxsplit=1)[1]
+        if not os.path.isdir(path_without_file):
+           raise FileNotFoundError("No existe el directorio de destino")
+    if len(new_filename) == 0:
+        os.rename(path, new_path)
+    else:
 
-    pass
 
 path = input("Introduce el archivo que quieres mover\n")
 new_path = input("Introduce ela ruta donde quieres moverlo\n")
